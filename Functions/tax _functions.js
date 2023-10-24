43c92ac7f4b97f0781c0c9b84358d64480853c21
NiyaWi <Niyaw144@gmail.com>
2023-10-24 16:08:14 -0400

    federal tax add 0 to 3

--- Functions/tax _functions.js
+++ Functions/tax _functions.js
@@ -9,3 +9,36 @@ function getSocSecTax(num){
 return afterTax
 }
 console.log("Social Security Tax:" + getSocSecTax(2500))
+
+
+function getMedicareTax(tax){
+
+    let postTax = tax*0.0145;
+    return postTax;
+}
+
+console.log("Medicare Tax:" + getMedicareTax(2500))
+
+function getFederalTax(grossPay,WithCode){
+if (WithCode === 0){
+    aftTax  = grossPay*0.23;
+}
+else if (WithCode =1){
+    aftTax = grossPay*0.21;
+}
+else if (WithCode = 2){
+    aftTax = grossPay*0.195
+}
+else if(WithCode = 3){
+    aftTax = grossPay*0.185
+}
+else if (WithCode = 4){
+    aftTax = grossPay*0.18 
+}
+
+    let taxWithAmnt  = aftTax;
+    
+    return taxWithAmnt
+}
+
+console.log("Fedral Tax Witheld:" + getFederalTax(2500,0))
