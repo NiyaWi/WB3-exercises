5e25b4e1e88d6463ecd12476be901904e3a8222a
NiyaWi <Niyaw144@gmail.com>
2023-10-27 13:42:39 -0400

    pt4

--- Scope/code_tests.js
+++ Scope/code_tests.js
@@ -1,12 +1,12 @@
 'use strict';
 let a = 4;
-function test3(){
-    a = 3;
+function test4(){
+    let a = 7; 
     console.log("a = " + a);
-}
-test3();
-console.log("a = " + a);
 
+}
+test4();
+console.log ("a = " + a);
 
 
 
